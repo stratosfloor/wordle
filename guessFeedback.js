781065c6cbcff70cd37e454dab42868476c8fad5
Emil Noren <stratosfloor@hotmail.com>
2022-03-10 14:15:44 +0100

    Not pretty, but working algorithm

--- guessFeedback.js
+++ guessFeedback.js
@@ -1,50 +1,39 @@
-//  Inputs: Guess & Answer
-//  Check letters
-//    - Loop through strings
-//    - If condition for correct, misplaced, incorrect. 
-//    - 
-//  Output: [
-//    {"A": "incorrect"},
-//    {"B": "correct"},
-//    {"C": "misplaced"}
-//   ]
-
-
 export function guessFeedback(guess, answer) {
+  const localAnswer = new Array(...new Set(answer)); 
+  console.log(localAnswer);
   const NUMBER_OF_LETTERS = 5;
   const result = [];
 
   for (let i = 0; i < NUMBER_OF_LETTERS; i++) {
-    // console.log(guess[i] + ' : ' + answer[i])
     const obj = {};
     let value = "";
     
     if (guess[i] === answer[i]) {
       value = 'correct';
+      let index = localAnswer.indexOf(guess[i]);
+      localAnswer.splice(index,1);
     }
-    else if (answer.includes(guess[i])) {
-      value = 'misplaced';
-    }
-    else {
+    else if (!answer.includes(guess[i])) {
       value = 'incorrect';
     }
-
-    // for (let j = 0; j < NUMBER_OF_LETTERS; j++) {
-    //   if (guess[i] === answer[j] && value === 'misplaced') {
-    //     value = 'incorrect';
-    //     console.log(guess[i] + ' : ' + answer[j])
-
-    //   }
-    // }
-
 
     obj.letter = guess[i];
     obj.result = value;
     result.push(obj);
   }
 
+  for (let j = 0; j < NUMBER_OF_LETTERS; j++) {
+    if (result[j].result === '' && localAnswer.includes(guess[j])) {
+      result[j].result = 'misplaced';
+    }
+    else if (result[j].result === '' && !(localAnswer.includes(guess[j]))) {
+      result[j].result = 'incorrect';
+    }
+  }
+  console.log(localAnswer)
   console.log(result);
   return result;
 }
 
 guessFeedback('HALLÅ', 'CYKLA');
+guessFeedback('AAAAA', 'BBAAB');
