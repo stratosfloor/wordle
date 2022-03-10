f1cfdd533ee2ad09f94db982f1d7179f6c8edd5f
Emil Noren <stratosfloor@hotmail.com>
2022-03-10 14:19:11 +0100

    Not pretty, but working algorithm

--- guessFeedback.js
+++ guessFeedback.js
@@ -1,5 +1,5 @@
 export function guessFeedback(guess, answer) {
-  const localAnswer = new Array(...new Set(answer)); 
+  const localAnswer = new Array(...new Set(answer));
   console.log(localAnswer);
   const NUMBER_OF_LETTERS = 5;
   const result = [];
@@ -7,14 +7,13 @@ export function guessFeedback(guess, answer) {
   for (let i = 0; i < NUMBER_OF_LETTERS; i++) {
     const obj = {};
     let value = "";
-    
+
     if (guess[i] === answer[i]) {
-      value = 'correct';
+      value = "correct";
       let index = localAnswer.indexOf(guess[i]);
-      localAnswer.splice(index,1);
-    }
-    else if (!answer.includes(guess[i])) {
-      value = 'incorrect';
+      localAnswer.splice(index, 1);
+    } else if (!answer.includes(guess[i])) {
+      value = "incorrect";
     }
 
     obj.letter = guess[i];
@@ -23,17 +22,16 @@ export function guessFeedback(guess, answer) {
   }
 
   for (let j = 0; j < NUMBER_OF_LETTERS; j++) {
-    if (result[j].result === '' && localAnswer.includes(guess[j])) {
-      result[j].result = 'misplaced';
-    }
-    else if (result[j].result === '' && !(localAnswer.includes(guess[j]))) {
-      result[j].result = 'incorrect';
+    if (result[j].result === "" && localAnswer.includes(guess[j])) {
+      result[j].result = "misplaced";
+    } else if (result[j].result === "" && !localAnswer.includes(guess[j])) {
+      result[j].result = "incorrect";
     }
   }
-  console.log(localAnswer)
+  console.log(localAnswer);
   console.log(result);
   return result;
 }
 
-guessFeedback('HALLÅ', 'CYKLA');
-guessFeedback('AAAAA', 'BBAAB');
+guessFeedback("HALLÅ", "CYKLA");
+guessFeedback("AAAAA", "BBAAB");
