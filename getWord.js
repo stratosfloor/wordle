13475d1fb501be818bbd5fee7b9de855da03bcca
Emil Noren <stratosfloor@hotmail.com>
2022-03-12 16:20:57 +0100

    Removed mockdata for testing

--- getWord.js
+++ getWord.js
@@ -1,107 +1,3 @@
-
-const data = [
-  "film",
-  "robust",
-  "dirt",
-  "rabbit",
-  "well-made",
-  "grease",
-  "ruin",
-  "push",
-  "dirty",
-  "crate",
-  "bless",
-  "mixed",
-  "town",
-  "prefer",
-  "snotty",
-  "depend",
-  "nosy",
-  "pizzas",
-  "dizzy",
-  "awesome",
-  "crowded",
-  "basin",
-  "ultra",
-  "smart",
-  "anger",
-  "sand",
-  "object",
-  "yard",
-  "nimble",
-  "complex",
-  "station",
-  "bore",
-  "vast",
-  "breezy",
-  "incandescent", 
-  "exist",
-  "obtain",
-  "coil",
-  "step",
-  "string",
-  "profuse",
-  "quilt",
-  "husky",
-  "scene",
-  "instinctive",
-  "wren",
-  "tree",
-  "bottle",
-  "scatter",
-  "brush",
-  "suit",
-  "sink",
-  "sneeze",
-  "overwrought",
-  "stroke",
-  "appliance",
-  "extend",
-  "library",
-  "distribution", 
-  "claim",
-  "unsuitable",
-  "boot",
-  "cagey",
-  "frightening",
-  "answer",
-  "excite",
-  "grin",
-  "gather",
-  "shrill",
-  "flagrant",
-  "optimal",
-  "scarecrow",
-  "laughable",
-  "unable",
-  "uneven",
-  "seal",
-  "save",
-  "cuddly",
-  "intend",
-  "heat",
-  "economic",
-  "rainstorm",
-  "ground",
-  "zipper",
-  "alarm",
-  "amazing",
-  "gaping",
-  "lame",
-  "peaceful",
-  "telling",
-  "fowl",
-  "sigh",
-  "pine",
-  "flashy",
-  "sparkle",
-  "curly",
-  "veil",
-  "cross",
-  "coach",
-  "development",
-]
-
 export function getWord(list, numberOfLetters, uniqueLetters = false) {
   let payload = [];
   if (!uniqueLetters) {
@@ -111,6 +7,7 @@ export function getWord(list, numberOfLetters, uniqueLetters = false) {
       .filter((word) => word.length == numberOfLetters)
       .filter((word) => numberOfLetters == new Set(word).size);
   }
+  console.log(payload);
   if (payload.length > 0) {
     return payload[Math.floor(Math.random() * payload.length)];
   } else {
@@ -118,9 +15,3 @@ export function getWord(list, numberOfLetters, uniqueLetters = false) {
     return null;
   }
 }
-
-
-
-const word = getWord(data, 10);
-console.log(word);
-
